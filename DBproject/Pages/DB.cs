ea0eed59548c643dceca3f0867ea96217ca7720c
nadamohamed1230 <152847465+nadamohamed1230@users.noreply.github.com>
2024-05-20 19:03:14 +0300

    .

--- DBproject/Pages/DB.cs
+++ DBproject/Pages/DB.cs
@@ -9,7 +9,7 @@ namespace DBproject.Pages
         public SqlConnection con { get; set; }
         public DB()
         {
-            string conStr = "Data Source=SQL6032.site4now.net;Initial Catalog=db_aa83e2_nadamohamed123001;Persist Security Info=True;User ID=db_aa83e2_nadamohamed123001_admin;Password=***********;Trust Server Certificate=True";
+            string conStr = "Data Source=SQL6032.site4now.net;User ID=db_aa83e2_nadamohamed123001_admin;Password=********;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
             con = new SqlConnection(conStr);
         }
 
