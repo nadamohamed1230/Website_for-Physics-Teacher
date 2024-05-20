4386085ba90be9efa63e5ce7a7e8159a133ee2bc
asharsakr <s-ashar.sakr@zewailcity.edu.eg>
2024-05-20 16:52:16 +0300

    connection

---
+++ DBproject/Pages/DB.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Numerics;
+
+namespace DBproject.Pages
+{
+    public class DB
+    {
+        public SqlConnection con { get; set; }
+        public DB()
+        {
+            string conStr = "Data Source=Silverstone;Initial Catalog=Survey;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+            con = new SqlConnection(conStr);
+        }
+
+    }
+}
