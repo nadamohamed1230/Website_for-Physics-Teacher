44c08b6e96ec11be7b5a04a290e36abc848efef6
nadamohamed1230 <152847465+nadamohamed1230@users.noreply.github.com>
2024-05-21 19:39:10 +0300

    test

---
+++ DBproject/Pages/video.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DBproject.Pages
+{
+    public class videoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
